f7eab3d9b6a712daffa5009cc49b05d2e111c962
cbjun <1656043947@qq.com>
2020-08-18 15:59:15 +0800

    mybatis day02

--- Mybatis-Study/Mybatis-Nothing/src/main/java/com/test/dao/UserMapper.java
+++ Mybatis-Study/Mybatis-Nothing/src/main/java/com/test/dao/UserMapper.java
@@ -3,6 +3,7 @@ package com.test.dao;
 import com.test.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author cbjun
@@ -13,4 +14,14 @@ public interface UserMapper {
     List<User> findUser();
 
     User getUserById(Integer id);
+
+    void addUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(Integer id);
+    //使用map代替实体类或者多个入参的情况
+    void addUserByMap(Map<String,Object> map);
+
+    List<User> findUserByNameLike(String name);
 }
--- Mybatis-Study/Mybatis-Nothing/src/test/java/test01.java
+++ Mybatis-Study/Mybatis-Nothing/src/test/java/test01.java
@@ -1,27 +1,106 @@
 import com.test.dao.UserMapper;
 import com.test.pojo.User;
 import com.test.utils.MyBatisUtils;
+import lombok.extern.java.Log;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author cbjun
  * @create 2020/8/17 15:35
  */
+@Log
 public class test01 {
 
     @Test
-    public void test01(){
+    public void test01() {
         SqlSession sqlSession = MyBatisUtils.getSqlSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        List<User> users = userMapper.findUser();
-        User user01 = userMapper.getUserById(1);
-        System.out.println(user01);
-        for(User user : users){
-            System.out.println(user);
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<User> users = userMapper.findUser();
+            User user01 = userMapper.getUserById(1);
+            System.out.println(user01);
+            users.forEach(e -> {
+                System.out.println(e);
+            });
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        } finally {
+            sqlSession.close();
         }
-        sqlSession.close();
+
+    }
+
+    @Test
+    //增删改需要提交事务
+    public void test02(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            userMapper.addUser(new User("王五","87713551"));
+            //提交事务
+            sqlSession.commit();
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void test03(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.getUserById(3);
+            user.setName("王虎");
+            user.setPassword("王虎666");
+            userMapper.updateUser(user);
+            userMapper.deleteUser(1);
+            //提交事务
+            sqlSession.commit();
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void test04(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            Map<String,Object> map  = new HashMap<String, Object>();
+            map.put("name","周建国");
+            map.put("password","I love China");
+            userMapper.addUserByMap(map);
+            //提交事务
+            sqlSession.commit();
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void test05(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<User> users = userMapper.findUserByNameLike("%"+"王"+"%");
+            users.forEach(e->{
+                System.out.println(e);
+            });
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        } finally {
+            sqlSession.close();
+        }
     }
 }
